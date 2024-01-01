228befa4e3fe2d94f6bed3eb8aa3f7401c3eb8a1
Gibril Idrisu Issaka <gibrilissakaiddrisu@gmail.com>
2024-01-01 16:46:27 +0000

    untrack some folders

--- task/models.py
+++ task/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import timezone
 from users.models import HODProfile, WorkersProfile
 
 
@@ -8,13 +7,15 @@ class Task(models.Model):
     type = models.CharField(max_length=20, null=True)
     description = models.CharField(max_length=1000000)
     progress = models.IntegerField(default=0)
-    done = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)
     createdBy = models.ForeignKey(HODProfile, on_delete=models.CASCADE, null=True)
     createdAt = models.DateTimeField(auto_now=True)
+    updatedAt = models.DateTimeField(auto_now_add=True)
     due_date = models.DateField(null=True)
 
 class WorkerTask(models.Model):
     worker = models.ForeignKey(WorkersProfile, on_delete=models.SET_NULL, null=True)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     createdBy = models.ForeignKey(HODProfile, on_delete=models.SET_NULL, null=True)
+
 
